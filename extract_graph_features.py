bc52dd4765f6f0dbb0f33b2b1392c311b697f205
Samip Thakkar <55044346+samip-thakkar@users.noreply.github.com>
2020-04-17 01:23:55 -0700

    Add graph features from Neo4j

---
+++ extract_graph_features.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Samip
+"""
+class GraphFeatures:
+
+    def extractGraphFeatures(self):
+        import pandas as pd
+        df = pd.read_csv("bs140513_032310.csv")
+        
+        
+        """ADDING GRAPH FEATURES"""
+        from py2neo import Graph
+        graph = Graph(password = ' ', bolt_port = 7687, http_port = 7474)
+        
+            
+        # Helper functions to add network features to input dataframe 
+        def add_degree(x):
+            return valueDict[x]['degree']
+        def add_community(x):
+            return str(valueDict[x]['community']) # cast to string for one-hot encoding
+        def add_pagerank(x):
+            return valueDict[x]['pagerank']
+        
+        # Read in a new dataframe and add netork features 
+        import pandas as pd
+        df = pd.read_csv("bs140513_032310.csv")
+        
+        query = """
+        MATCH (p:Placeholder)
+        RETURN p.id AS id, p.degree AS degree, p.pagerank as pagerank, p.community AS community 
+        """
+        
+        data = graph.run(query)
+        valueDict = {}
+        for d in data:
+            valueDict[d['id']] = {'degree': d['degree'], 'pagerank': d['pagerank'], 'community': d['community']}
+        
+        
+        df['merchDegree'] = df.merchant.apply(add_degree)
+        df['custDegree'] = df.customer.apply(add_degree)
+        df['custPageRank'] = df.customer.apply(add_pagerank)
+        df['merchPageRank'] = df.merchant.apply(add_pagerank)
+        df['merchCommunity'] = df.merchant.apply(add_community)
+        df['custCommunity'] = df.customer.apply(add_community)
+        
+        df.to_csv('bs140513_032310.csv')
